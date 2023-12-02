990e864c341a5d52ed3734308d411dcaa8e7ab0d
UnikMask <alexandre.guerville@hotmail.com>
2023-12-02 21:01:16 +0100

    refactor(day02): Cleanup

--- src/day02.rs
+++ src/day02.rs
@@ -2,36 +2,23 @@ use aoc::load_file;
 
 fn main() {
     let contents = load_file("resources/day02/actual.txt");
-    let setup: Vec<u32> = vec![12, 13, 14];
-    let get_index = |color: &str| match color {
-        "green" => 1,
-        "blue" => 2,
-        _ => 0,
-    };
+    let setup: [u32; 3] = [12, 13, 14];
+    let get_index = |color: &str| match color {"green" => 1, "blue" => 2, _ => 0};
 
-    // Parse content
     let (mut sum1, mut sum2) = (0, 0);
     for game in contents {
         let parts: Vec<&str> = game.split(": ").collect();
-        let (mut minv, mut bad): (Vec<u32>, bool) = (vec![0, 0, 0], false);
+        let (mut minv, mut bad): ([u32; 3], bool) = ([0, 0, 0], false);
         for round in parts[1].split("; ") {
             for drawn_ball in round.split(", ") {
                 let drawn: Vec<&str> = drawn_ball.split_whitespace().collect();
                 let (quantity, i) = (drawn[0].parse().unwrap(), get_index(drawn[1]));
-                if quantity > setup[i] {
-                    bad = true;
-                }
+                bad |= quantity > setup[i];
                 minv[i] = std::cmp::max(minv[i], quantity);
             }
         }
         if !bad {
-            sum1 += parts[0]
-                .split(" ")
-                .nth(1)
-                .unwrap()
-                .to_string()
-                .parse::<u32>()
-                .unwrap();
+            sum1 += parts[0].split_whitespace().nth(1).unwrap().to_string().parse::<u32>().unwrap();
         }
         sum2 += minv.iter().map(|i| *i).product::<u32>();
     }
