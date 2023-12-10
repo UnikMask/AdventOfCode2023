591b8f69e9e53bb74bb0e7b56481c20ea19f2746
UnikMask <alexandre.guerville@hotmail.com>
2023-12-10 14:23:25 +0100

    refactor(day09): Memory optimization by only keeping 1st & last of each history transforms

--- src/day09.rs
+++ src/day09.rs
@@ -4,21 +4,17 @@ fn main() {
     println!("Result: {:?}", load_file("resources/day09/actual.txt")
         .iter()
         .map(|line| {
-            let mut transforms = vec![line
+            let mut cur= line
                 .split_whitespace()
                 .map(|num| num.parse::<i64>().unwrap())
-                .collect::<Vec<i64>>()];
+                .collect::<Vec<i64>>();
+            let mut transforms = vec![(*cur.first().unwrap(), *cur.last().unwrap())];
 
-            while !transforms.last().unwrap().iter().all(|i| *i == 0) {
-                let cur = transforms.last().unwrap().clone();
-                transforms.push((0..cur.len() - 1).map(|i| cur[i + 1] - cur[i]).collect());
+            while !cur.iter().all(|i| *i == 0) {
+                cur = (0..cur.len() - 1).map(|i| cur[i + 1] - cur[i]).collect();
+                transforms.push((*cur.first().unwrap(), *cur.last().unwrap()));
             }
-            transforms
-                .iter()
-                .rev()
-                .map(|t| (*t.first().unwrap(), *t.last().unwrap()))
-                .reduce(|res, t| (t.0 - res.0, t.1 + res.1))
-                .unwrap()
+            transforms.iter().rev().map(|i| *i).reduce(|res, t| (t.0 - res.0, t.1 + res.1)).unwrap()
         })
         .reduce(|a, b| (a.0 + b.0, a.1 + b.1))
         .unwrap());
